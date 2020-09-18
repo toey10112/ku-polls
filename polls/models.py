cdcb033cb6f9fe76dc43276f14e87a4f46a4f144
Suchon Prasert <58396402+toey10112@users.noreply.github.com>
2020-09-19 01:45:18 +0700

    add is_published and can_vote

--- polls/models.py
+++ polls/models.py
@@ -2,24 +2,41 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-class Question(models.Model):
 
+class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    end_date = models.DateTimeField('Date that polls expires')
 
     def __str__(self):
         return self.question_text
+
+    def is_published(self):
+        now = timezone.now()
+        if now >= self.pub_date:
+            return True
+        else:
+            return False
+
+    def can_vote(self):
+        now = timezone.now()
+        if self.pub_date <= now <= self.end_date:
+            return True
+        else:
+            return False
 
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+    was_published_recently.admin_order_field = 'end_date'
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
 
-class Choice(models.Model):
 
+
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
