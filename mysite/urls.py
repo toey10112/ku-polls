1a31058be8a4f4b8effc25c4800085303a468ee9
Suchon Prasert <58396402+toey10112@users.noreply.github.com>
2020-09-19 15:42:50 +0700

    fix address

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    # path('',poll.views,index,name ="main index"),
+    path('', include('polls.urls')),
+
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
